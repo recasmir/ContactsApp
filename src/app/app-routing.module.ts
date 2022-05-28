70e343713d974abef4912f8978369202113c71f7
recasmir <mireia.recasens@gmail.com>
2022-05-28 11:40:15 +0200

    created folding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SingleContactComponent } from './components/single-contact/single-contact.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'contact/:id', component:SingleContactComponent},
+  {path:'**', redirectTo:''},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/components.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SearchComponent } from './search/search.component';
+import { SingleContactComponent } from './single-contact/single-contact.component';
+
+
+
+@NgModule({
+  declarations: [
+    SearchComponent,
+    SingleContactComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ComponentsModule { }
